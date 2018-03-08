339fbbac08766c7ce0bd69d52590e05974fc8ec6
Ganesh Prasad <ganesh@grexit.com>
2018-03-08 14:51:16 +0530

    refresh study

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {selectBook} from '../actions/index';
+import {selectBook} from '../actions/index'; // import action(s) . Creates a dispatch element in BookList. 
 import {bindActionCreators} from 'redux';
 
 class BookList extends Component{
@@ -10,7 +10,7 @@ class BookList extends Component{
             return (
                 <li 
                     key={book.title}
-                    onClick={ () => this.props.selectBook(book) }
+                    onClick={ () => this.props.selectBook(book) } //this refers to BookList cause of () =>
                     className="list-group-item">
                     {book.title}
                 </li>
@@ -28,14 +28,18 @@ class BookList extends Component{
 
 function mapStateToProps(state){
     return {
-        books : state.books    
+        books : state.books
     };
 }
 
+// This function to use selectBook in BookList ( Passed to connect ).
 function mapDispatchToProps(dispatch){
-    // bindActionCreators -> makes action flow through reducers
+    // bindActionCreators -> makes action flow through reducers.
+    // Ok.
     return bindActionCreators({selectBook : selectBook}, dispatch);
 }
 
-// connect connects 
+// connect connects a function returning data to BookList ( A react Component ).
+// Any data we need in props variable can be sent using connect.
+// Weird.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
